437a342c4106e3599488a26c6ef3b4b8779cc061
ptrt <peter.tumfart@gmx.at>
2022-05-04 18:03:53 +0200

    Implemented paradigm. Added read serial to the paradigm folder. Small change in read_serial (return char if timeout).

--- 02_paradigm/paradigm.m
+++ 02_paradigm/paradigm.m
@@ -13,17 +13,29 @@ markers.break = {'Break'};
 markers.cue = {'Cue'};
 markers.start = {'Start'};
 markers.classes = classes;
+markers.release = {'Release'};
+markers.touch = {'Touch'};
 
 %% Setup timing of paradigm IN SECONDS
 timings = struct;
-% Time of the distance LED:
-timings.imag_period = 2;
+% Time between indication and reach
+timings.indication = 2;
 % Reaching duration:
-timings.fb_rest = 5;
+timings.reach = 5;
 % Minimum pause duration
 timings.min_break = 3;
 % time variability of break
 timings.break_variation = 1;
+
+
+%% Create sound:
+fs_sound = 8912;
+f = 400;
+dt = 1/fs_sound;
+stop_time = 0.15;
+t = 0:dt:stop_time;
+
+beep_sine = sin(2*pi*f*t);
 
 
 %% generic checks
@@ -32,9 +44,72 @@ N_classes = length(classes);
 classes_nrTrials = repmat(trials_perclass, 1, N_classes);
 N_trials = sum(classes_nrTrials);
 
+%% Serial commands for params
+cmds = ['a', 'b']; % led on, led off, read photoresistor
+indicator_positions = ['l', 'l', 'r', 'r', 't', 't', 'b', 'b'];
+reaching_positions = ['c', 'r', 'c', 'l', 'c', 'b', 'c', 't'];
 
+%% Create parameter set of types of labels and number of trials per label and
+% markers and cues
+params = struct;
 
-% Connect to serial:
+for iClass = 1: N_classes
+    params(iClass).label = classes(iClass);
+    params(iClass).trials = classes_nrTrials(iClass);
+    params(iClass).marker = markers.classes(iClass);
+    params(iClass).indicator_on = [cmds(1) ' ' indicator_positions(iClass)];
+    params(iClass).indicator_off = [cmds(2) ' ' indicator_positions(iClass)];
+    params(iClass).reaching_on = [cmds(1) ' ' reaching_positions(iClass)];
+    params(iClass).reaching_off = [cmds(2) ' ' reaching_positions(iClass)];
+    %params(iClass).cue = imread(char(strcat(folder_images,images(iClass))));
+end
+
+clearvars iClass
+
+%% create pseudorandom order of trials
+trial_labels = [];
+trial_ind_on = [];
+trial_ind_off = [];
+trial_reach_on = [];
+trial_reach_off = [];
+for iClass = 1:N_classes
+    trial_labels = [trial_labels; iClass*ones(params(iClass).trials,1)];
+    trial_ind_on = [trial_ind_on; repmat(params(iClass).indicator_on,[5 1])];
+    trial_ind_off = [trial_ind_off; repmat(params(iClass).indicator_off,[5 1])];
+    trial_reach_on = [trial_reach_on; repmat(params(iClass).reaching_on,[5 1])];
+    trial_reach_off = [trial_reach_off; repmat(params(iClass).reaching_off,[5 1])];
+end
+
+% check for 3 consecutive
+trp = inf;
+while trp ~= 0
+    perm_idcs = randperm(length(trial_labels));
+    trial_labels = trial_labels(perm_idcs);
+    trp = 0;
+    for iTrial = 4:N_trials
+        if (trial_labels(iTrial) == trial_labels(iTrial-1)) && (trial_labels(iTrial-1) == trial_labels(iTrial-2))
+            trp = trp + 1;
+            break
+        end
+    end
+end
+trial_ind_on = trial_ind_on(perm_idcs,:);
+trial_ind_off = trial_ind_off(perm_idcs,:);
+trial_reach_on = trial_reach_on(perm_idcs,:);
+trial_reach_off = trial_reach_off(perm_idcs,:);
+
+%% Connect to serial:
+s = serialport("COM8",115200,"Timeout",5);
+s.Terminator;
+configureTerminator(s,"CR");
+
+pause(3);
+
+% Wait until the initial command is received:
+read_val = [];
+while s.NumBytesAvailable > 0
+    read_val = [read_val, readline(s)];
+end
 
 
 %% Start LSL stream:
@@ -48,3 +123,125 @@ info = lsl_streaminfo(lib,'markers-paradigm','Marker',1,0,'cf_string','markers_p
 
 disp('Opening an outlet...');
 markers_out = lsl_outlet(info);
+
+
+%% Ask for validation before starting
+while(1)
+    reply = input('Connect all streams and start recording in LabRecorder!!!\n Press Enter to continue:','s');
+    if isempty(reply)
+        break
+    end
+end
+disp('Starting...');
+pause(7);
+
+%% Trials
+for iTrial = 1:N_trials
+    trial_type = trial_labels(iTrial);
+    markers_out.push_sample(markers.classes(trial_type));
+    fprintf('trial: %d   class: %s \n',iTrial, char(classes(trial_type))  )
+    
+    %% INDICATION PERIOD:
+    % Turn on the correct LED:
+    writeline(s,trial_ind_on(iTrial,:));
+    % Get acknowledge:
+    ret_val = read_serial(s,1);
+    if ret_val == 'x'
+        fprintf('Error in serial communication!!!\n');
+        break;
+    end
+    % Send start marker
+    markers_out.push_sample(markers.start);
+    % Wait between indication and reach and send marker when touched/released:
+    t_start = tic;
+    while toc(t_start) < timings.indication
+        if s.NumBytesAvailable > 0
+            read_val = readline(s);
+            if read_val{1}(5) == '0'
+                markers_out.push_sample(markers.touch);
+                disp('Touch')
+            else
+                markers_out.push_sample(markers.release);
+                disp('Release')
+            end
+        end
+    end
+    disp(toc(t_start))
+    
+    %% RECHING PERIOD:
+    % Turn off the indication LED:
+    writeline(s,trial_ind_off(iTrial,:));
+    % Get acknowledge:
+    ret_val = read_serial(s,1);
+    if ret_val == 'x'
+        fprintf('Error in serial communication!!!\n');
+        break;
+    end
+    % Turn on the reaching LED:
+    writeline(s,trial_reach_on(iTrial,:));
+    % Get acknowledge:
+    ret_val = read_serial(s,1);
+    if ret_val == 'x'
+        fprintf('Error in serial communication!!!\n');
+        break;
+    end
+    % Send reaching cue is shown marker:
+    markers_out.push_sample(markers.cue);
+    % Wait for reaching period and send marker when touched/released:
+    t_start = tic;
+    while toc(t_start) < timings.reach
+        if s.NumBytesAvailable > 0
+            read_val = readline(s);
+            if read_val{1}(5) == '0'
+                markers_out.push_sample(markers.touch);
+                disp('Touch')
+            else
+                markers_out.push_sample(markers.release);
+                disp('Release')
+            end
+        end
+    end
+    disp(toc(t_start))
+    
+    % Turn off reaching LED:
+    writeline(s,trial_reach_off(iTrial,:));
+    % Get acknowledge:
+    ret_val = read_serial(s,1);
+    if ret_val == 'x'
+        fprintf('Error in serial communication!!!\n');
+        break;
+    end
+    
+    %% BREAK PERIOD:
+    % Signalise end with beep:
+    sound(beep_sine);
+    % Send break indication marker:
+    markers_out.push_sample(markers.break);
+    % Wait for break period and send marker when touched/released:
+    break_time = timings.min_break + timings.break_variation * rand;
+    t_start = tic;
+    while toc(t_start) < break_time
+        if s.NumBytesAvailable > 0
+            read_val = readline(s);
+            if read_val{1}(5) == '0'
+                markers_out.push_sample(markers.touch);
+                disp('Touch')
+            else
+                markers_out.push_sample(markers.release);
+                disp('Release')
+            end
+        end
+    end
+    disp(toc(t_start)) 
+    
+
+end
+
+display('End of Experiment. Please stop recording.');
+
+%%
+clear lib
+pause(0.5)
+clear markers_out
+clear s
+
--- 02_paradigm/read_serial.m
+++ 02_paradigm/read_serial.m
@@ -1,13 +1,12 @@
 function read_val = read_serial(serial, timeout)
-    tic
+    start = tic;
     while 1==1
         read_val = [];
         if serial.NumBytesAvailable > 0
             read_val = [read_val, readline(serial)];
-            toc
             break;
         end   
-        if toc > timeout
+        if toc(start) > timeout
             read_val = 'x';
             break; 
         end
