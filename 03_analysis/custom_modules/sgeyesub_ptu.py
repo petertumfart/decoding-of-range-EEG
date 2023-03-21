bccea2317a49b765f25fae2d5d700e3d1b0be5be
ptrt <peter.tumfart@gmx.at>
2023-03-21 16:22:52 +0100

    Added epoching.

--- 03_analysis/custom_modules/sgeyesub_ptu.py
+++ 03_analysis/custom_modules/sgeyesub_ptu.py
@@ -1,11 +1,11 @@
 import os
 import mne
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import datetime
 from datetime import datetime, timezone
 from scipy.stats import t
+
 
 def calc_eye_derivations(src, dst, sbj):
     """
@@ -75,3 +75,46 @@ def lp_filter_derivatives(src, dst, sbj):
     # Store the filtered file:
     store_name = dst + '/' + sbj + '_' + 'eye' + '_lp_filtered_raw.fif'
     raw.save(store_name, overwrite=True)
+
+
+def epoch_eye_paradigm(src, dst, sbj, mrks):
+    """
+   Epoch the EEG data for a given subject in the eye paradigm.
+
+   :param src: The path to the directory containing the raw EEG data.
+   :type src: str
+   :param dst: The path to the directory where the epoched data will be stored.
+   :type dst: str
+   :param sbj: The subject identifier to be used in the output file name.
+   :type sbj: str
+   :param mrks: A dictionary containing the markers of interest and their corresponding IDs.
+   :type mrks: list
+   """
+
+    mne.set_log_level('INFO')
+
+    # There can be only one file  with matching conditions since we are splitting in folders:
+    f_name = [f for f in os.listdir(src) if (sbj in f) and ('eye' in f)][0]
+
+    file = src + '/' + f_name
+    raw = mne.io.read_raw(file, preload=True)
+
+    # Extract epochs:
+    events_from_annot, event_dict = mne.events_from_annotations(raw)
+
+    # Get event dict:
+    event_dict_of_interest = _get_subset_of_dict(event_dict, mrks)
+
+    # Epoch the data:
+    epochs = mne.Epochs(raw, events_from_annot, event_id=event_dict_of_interest, tmin=1.0, tmax=9.0,
+                        baseline=None, reject_by_annotation=True, preload=True, picks=['eeg', 'eog'])
+
+    # Store the epoched file:
+    store_name = dst + '/' + sbj + '_' + 'eye' + '_epoched_for_eyesub.fif'
+    epochs.save(store_name, overwrite=True)
+
+    mne.set_log_level('WARNING')
+
+
+def _get_subset_of_dict(full_dict, keys_of_interest):
+    return dict((k, full_dict[k]) for k in keys_of_interest if k in full_dict)
